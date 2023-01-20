c509f11d1624bdfab08d1bab9937b6ac07e53629
Tomáš Procházka <tomas.prochazka5d@gmail.com>
2023-01-20 13:30:16 +0100

    wrap error only for >=go1.13

--- protocol.go
+++ protocol.go
@@ -104,8 +104,7 @@ func (t tcpDialer) DialSqlConnection(ctx context.Context, c *Connector, p *msdsn
 	}
 	// Can't do the usual err != nil check, as it is possible to have gotten an error before a successful connection
 	if conn == nil {
-		f := "unable to open tcp connection with host '%v:%v': %w"
-		return nil, fmt.Errorf(f, p.Host, resolveServerPort(p.Port), err)
+		return nil, wrapConnErr(p, err)
 	}
 	if p.ServerSPN == "" {
 		p.ServerSPN = generateSpn(p.Host, instanceOrPort(p.Instance, p.Port))
---
+++ protocol_go113pre.go
@@ -0,0 +1,9 @@
+//go:build !go1.13
+// +build !go1.13
+
+package mssql
+
+func wrapConnErr(p *msdsn.Config, err error) error {
+	f := "unable to open tcp connection with host '%v:%v': %v"
+	return fmt.Errorf(f, p.Host, resolveServerPort(p.Port), err)
+}
