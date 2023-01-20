af7a825ccf2b07d34e259db4810490f4cd6fff8c
Tomáš Procházka <tomas.prochazka5d@gmail.com>
2023-01-20 13:42:27 +0100

    fix imports for go1.13pre

--- protocol_go113pre.go
+++ protocol_go113pre.go
@@ -3,6 +3,12 @@
 
 package mssql
 
+import (
+	"fmt"
+
+	"github.com/microsoft/go-mssqldb/msdsn"
+)
+
 func wrapConnErr(p *msdsn.Config, err error) error {
 	f := "unable to open tcp connection with host '%v:%v': %v"
 	return fmt.Errorf(f, p.Host, resolveServerPort(p.Port), err)
